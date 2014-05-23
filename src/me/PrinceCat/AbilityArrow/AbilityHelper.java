5d573b7e9b2014dbb341efeb6fd1fdcfe02efd04
Liam Wilkinson <liam.wilkinson@gmail.com>
2014-05-24 02:32:27 +1000

    Update AbilityHelper.java

--- src/me/PrinceCat/AbilityArrow/AbilityHelper.java
+++ src/me/PrinceCat/AbilityArrow/AbilityHelper.java
@@ -3,17 +3,19 @@ package me.PrinceCat.AbilityArrow;
 import java.util.HashMap;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class AbilityHelper {
 
 	public static HashMap<String, String> playerAbility = new HashMap<String, String>();
-	
+
 	// Set player's current ability
 	public static void setAbility(String playerName, String abilityName) {
 		playerAbility.put(playerName, abilityName);
 	}
-	
+
 	// Get player's current ability
 	public static String getAbility(String playerName) {
 		if (playerAbility.containsKey(playerName)) {
@@ -21,19 +23,19 @@ public class AbilityHelper {
 		}
 		return null;
 	}
-	
+
 	// Remove player from ability HashMap
 	public static void resetAbility(String playerName) {
 		if (playerAbility.containsKey(playerName)) {
 			playerAbility.remove(playerName);
 		}
 	}
-	
+
 	// Toggle a player's selected ability
 	public static void toggleAbility(Player player, String abilityName) {
 		String playerName = player.getName();
 		String selectedAbility = getAbility(playerName);
-		
+
 		if (selectedAbility != null) {
 			if (selectedAbility.equalsIgnoreCase(abilityName)) {
 				player.sendMessage(ChatColor.RED + abilityName + " disabled.");
@@ -47,4 +49,19 @@ public class AbilityHelper {
 		}
 	}
 	
+	@SuppressWarnings("deprecation")
+	public static void removeAbilityItem(Player player, Material material, short damageValue) {
+		for (int i = 0; i <= player.getInventory().getSize(); i++) {
+			if (player.getInventory().getItem(i).getType().equals(material)) {
+				System.out.print("Item is material!");
+				if (player.getInventory().getItem(i).getDurability() == damageValue) {
+					System.out.print("Item has damage value!");
+					player.getInventory().removeItem(new ItemStack(player.getInventory().getItem(i).getType(), 1, damageValue));
+					break;
+				}
+			}
+		}
+		
+	}
+
 }
