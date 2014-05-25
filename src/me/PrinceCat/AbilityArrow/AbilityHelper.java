ea4bcdfbcdcbf1cf2bd911eb56c24dc985d186d4
Liam Wilkinson <liam.wilkinson@gmail.com>
2014-05-25 20:33:33 +1000

    Update AbilityHelper.java

--- src/me/PrinceCat/AbilityArrow/AbilityHelper.java
+++ src/me/PrinceCat/AbilityArrow/AbilityHelper.java
@@ -2,6 +2,8 @@ package me.PrinceCat.AbilityArrow;
 
 import java.util.HashMap;
 
+import me.PrinceCat.AbilityArrow.Abilities.Ability;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -9,15 +11,15 @@ import org.bukkit.inventory.ItemStack;
 
 public class AbilityHelper {
 
-	public static HashMap<String, String> playerAbility = new HashMap<String, String>();
+	public static HashMap<String, Ability> playerAbility = new HashMap<String, Ability>();
 
 	// Set player's current ability
-	public static void setAbility(String playerName, String abilityName) {
+	public static void setAbility(String playerName, Ability abilityName) {
 		playerAbility.put(playerName, abilityName);
 	}
 
 	// Get player's current ability
-	public static String getAbility(String playerName) {
+	public static Ability getAbility(String playerName) {
 		if (playerAbility.containsKey(playerName)) {
 			return playerAbility.get(playerName);
 		}
@@ -32,45 +34,39 @@ public class AbilityHelper {
 	}
 
 	// Toggle a player's selected ability
-	public static void toggleAbility(Player player, String abilityName) {
+	public static void toggleAbility(Player player, Ability abilityName) {
 		String playerName = player.getName();
-		String selectedAbility = getAbility(playerName);
-
-		if (selectedAbility != null) {
-			if (selectedAbility.equalsIgnoreCase(abilityName)) {
-				player.sendMessage(ChatColor.RED + abilityName + " disabled.");
-				resetAbility(playerName);
-			} else {
-				player.sendMessage(ChatColor.GREEN + abilityName + " enabled.");
-			}
-		} else {
-			player.sendMessage(ChatColor.GREEN + abilityName + " enabled.");
+		Ability selectedAbility = getAbility(playerName);
+		
+		if (selectedAbility == null) {
+			player.sendMessage(ChatColor.GREEN + abilityName.getUsableName() + " enabled.");
 			setAbility(playerName ,abilityName);
+			return;
+		}
+		
+		String usableName = selectedAbility.getUsableName();
+
+		if (selectedAbility.equals(abilityName)) {
+			player.sendMessage(ChatColor.RED + usableName + " disabled.");
+			resetAbility(playerName);
+		} else {
+			player.sendMessage(ChatColor.GREEN + usableName + " enabled.");
 		}
 	}
 	
-	//TODO Fix bug where item is never fully removed from inventory?
-	
-	public static void removeAbilityItem(Player player, Material material, short damageValue) {
-		if (player.getInventory().contains(new ItemStack(material, 1, damageValue))) {
-			System.out.print("Inventory has item!");
-			//player.getInventory().removeItem(new ItemStack(material, 1, damageValue));
-		}
-		
-		/*
+	public static void removeAbilityItem(Player player, Material material, short damage) {
+		//Very messy fix, but works?
 		for (int i = 0; i <= player.getInventory().getSize(); i++) {
-			if (player.getInventory().getItem(i).getType().equals(material)) {
-				System.out.print("Item is material!");
-				if (player.getInventory().getItem(i).getDurability() == damageValue) {
-					System.out.print("Item has damage value!");
-					player.getInventory().removeItem(new ItemStack(player.getInventory().getItem(i).getType(), 1, damageValue));
-					player.getInventory().
-					break;
+			if (player.getInventory().getItem(i).getType().equals(material) && player.getInventory().getItem(i).getDurability() == damage) {
+				int itemAmount = player.getInventory().getItem(i).getAmount();
+				if (itemAmount > 1) {
+					player.getInventory().getItem(i).setAmount(itemAmount - 1);
+				} else {
+					player.getInventory().removeItem(new ItemStack(player.getInventory().getItem(i)));
 				}
+				return;
 			}
 		}
-		*/
-		
 	}
 
 }
