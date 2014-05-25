2ebf9978ffca702070da1a3fdcf2bbfeeba0ffe5
Liam Wilkinson <liam.wilkinson@gmail.com>
2014-05-25 17:38:18 +1000

    Update FeatherAbility.java

--- src/me/PrinceCat/AbilityArrow/Abilities/FeatherAbility.java
+++ src/me/PrinceCat/AbilityArrow/Abilities/FeatherAbility.java
@@ -10,7 +10,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class FeatherAbility extends AbilityHelper{
